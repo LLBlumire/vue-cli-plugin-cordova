75dffdce2f2af83d4595536062a110d213ad857e
Lucille Blumire <llblumire@gmail.com>
2019-04-18 01:13:03 +0100

    Fix support for Electron

--- defaults.js
+++ defaults.js
@@ -2,6 +2,6 @@ module.exports = {
   cordovaPath: 'src-cordova',
   id: 'com.vue.example.app',
   appName: 'VueExampleAppName',
-  platforms: ['android', 'ios', 'browser', 'osx', 'cordova'],
+  platforms: ['android', 'ios', 'browser', 'osx', 'electron'],
   routerMode: 'hash'
 }
